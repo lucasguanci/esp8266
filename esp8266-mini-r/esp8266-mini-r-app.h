a7dda428bdd0e7a2e16e56b08d761be2194e9331
Luca Sguanci <luca.sguanci@gmail.com>
2022-10-06 10:08:22 +0200

    Add files via upload

---
+++ esp8266-mini-r/esp8266-mini-r-app.h
@@ -0,0 +1,182 @@
+/* paste here content of src/esp8266-mini-r-app.html */
+const char webpageCode[] = 
+R"=====(
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="WebSocket interface for a ESP8266-controlled robot">
+  <meta name="author" content="Luca Sguanci">
+  <title>:: mini-R :: WebSocket interface for a Robot</title>
+  <style>
+    .robot-cnt {
+      margin: 1em auto;
+    }
+    table {
+      text-align: center;
+    }
+    h3 {
+      font-size: 120%;
+      text-align: center;
+      margin: 2em auto;
+    }
+    .row {
+      margin-bottom: 1em;
+    }
+    .col-sm {
+      text-align: center;
+    }
+    button {
+      margin-top: 2em;
+      padding: 1em;
+      font-weight: bold;
+    }
+  </style>
+</head>
+
+<body>
+  <h3>Robot Control</h3>
+  <div class="container">
+    <table class="robot-cnt">
+      <thead></thead>
+      <tbody>
+        <tr>
+          <td></td>
+          <td>
+            <button type="button" class="btn" id="btnForward">Forward</button>
+          </td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>
+            <button type="button" class="btn" id="btnLeft">Left</button>
+          </td>
+          <td>
+            <button type="button" class="btn" id="btnStop">Stop</button>
+          </td>
+          <td>
+            <button type="button" class="btn" id="btnRight">Right</button>
+          </td>
+        </tr>
+        <tr>
+          <td></td>
+          <td>
+            <button type="button" class="btn" id="btnBackward">Backward</button>
+          </td>
+          <td></td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+  <!-- JS -->
+  <!-- if needed include jQuery, Popper and Bootstrap -->
+  <!-- Custom JS code -->
+  <script>
+    // comment to work disconnected from the ESP8266
+    var connection = new WebSocket('ws://' + location.hostname + ':81/', ['arduino']);
+    // uncomment to work disconnected from the ESP8266
+    // var connection = new WebSocket('ws://localhost:81/', ['arduino']);
+    connection.onopen = function () {
+      connection.send('Connect ' + new Date());
+    };
+    connection.onerror = function (error) {
+      console.log('WebSocket Error ', error);
+    };
+    connection.onmessage = function (e) {
+      console.log('Server: ', e.data);
+    };
+    connection.onclose = function () {
+      console.log('WebSocket connection closed');
+    };
+
+    // forward
+    const btnForward = document.getElementById("btnForward");
+    btnForward.addEventListener("mousedown", function(e) {
+      console.log("moving fw.. (mouse)");
+      connection.send("rForward");
+    });
+    btnForward.addEventListener("mouseup", function(e) {
+      console.log("halting robot (mouse)");
+      connection.send("rStop");
+    });
+    btnForward.addEventListener("touchstart", function(e) {
+      e.preventDefault();
+      console.log("moving fw.. (touch)");
+      connection.send("rForward");
+    });
+    btnForward.addEventListener("touchend", function(e) {
+      e.preventDefault();
+      console.log("halting robot (touch)");
+      connection.send("rStop");
+    });
+    // left
+    const btnLeft = document.getElementById("btnLeft");
+    btnLeft.addEventListener("mousedown", function(e) {
+      console.log("turning left..");
+      connection.send("rLeft");
+    });
+    btnLeft.addEventListener("mouseup", function(e) {
+      console.log("halting robot");
+      connection.send("rStop");
+    });
+    btnLeft.addEventListener("touchstart", function(e) {
+      e.preventDefault();
+      console.log("moving left.. (touch)");
+      connection.send("rLeft");
+    });
+    btnLeft.addEventListener("touchend", function(e) {
+      e.preventDefault();
+      console.log("halting robot (touch)");
+      connection.send("rStop");
+    });
+    // right
+    const btnRight = document.getElementById("btnRight");
+    btnRight.addEventListener("mousedown", function(e) {
+      console.log("turning right..");
+      connection.send("rRight");
+    });
+    btnRight.addEventListener("mouseup", function(e) {
+      console.log("halting robot");
+      connection.send("rStop");
+    });
+    btnRight.addEventListener("touchstart", function(e) {
+      e.preventDefault();
+      console.log("moving right.. (touch)");
+      connection.send("rRight");
+    });
+    btnRight.addEventListener("touchend", function(e) {
+      e.preventDefault();
+      console.log("halting robot (touch)");
+      connection.send("rStop");
+    });
+    // backward
+    const btnBackward = document.getElementById("btnBackward");
+    btnBackward.addEventListener("mousedown", function(e) {
+      console.log("moving backward..");
+      connection.send("rBackward");
+    });
+    btnBackward.addEventListener("mouseup", function(e) {
+      console.log("halting robot");
+      connection.send("rStop");
+    });
+    btnBackward.addEventListener("touchstart", function(e) {
+      e.preventDefault();
+      console.log("moving bw.. (touch)");
+      connection.send("rBackward");
+    });
+    btnBackward.addEventListener("touchend", function(e) {
+      e.preventDefault();
+      console.log("halting robot (touch)");
+      connection.send("rStop");
+    });
+    // stop
+    const btnStop = document.getElementById("btnStop");
+    btnStop.addEventListener("click", function(e) {
+      console.log("halting robot..");
+      connection.send("rStop");
+    });
+  </script>
+</body>
+</html>
+)=====";
---
+++ esp8266-mini-r/esp8266-mini-r.ino
@@ -0,0 +1,177 @@
+/*
+  luca sguanci – idrovolante.org
+  january/february, 2022
+  
+  https://github.com/lucasguanci/esp8266
+  
+  based on Adafruit ESP8266 Feather Huzzah and Adafruit Modtor Shield, see:
+  - https://learn.adafruit.com/bluefruit-feather-robot
+  - https://www.adafruit.com/product/2939
+*/
+
+// AF Motor Shield
+#include <Wire.h>
+#include <Adafruit_MotorShield.h>
+// ESP8266 
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <WebSocketsServer.h>
+// Include web-app
+#include "esp8266-mini-r-app.h"
+
+// Create the motor shield object with the default I2C address
+Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
+
+// And connect 2 DC motors to port M3 & M4 !
+Adafruit_DCMotor *L_MOTOR = AFMS.getMotor(3);
+Adafruit_DCMotor *R_MOTOR = AFMS.getMotor(4);
+
+// Create a webserver object that listens for HTTP request on port 80
+ESP8266WebServer server(80);
+// create a websocket server on port 81
+WebSocketsServer webSocket(81);
+
+// The name of the Wi-Fi network that will be created
+const char *ssid = "alcyone";
+// The password required to connect to it, leave blank for an open network
+const char *password = "";
+
+// Commands send through WebSocket
+static const char rFW[] = "rForward";
+static const char rBW[] = "rBackward";
+static const char rLT[] = "rLeft";
+static const char rRT[] = "rRight";
+static const char rST[] = "rStop";
+
+// functions
+void startMotors() {
+  L_MOTOR->setSpeed(0);
+  L_MOTOR->run(RELEASE);
+
+  R_MOTOR->setSpeed(0);
+  R_MOTOR->run(RELEASE);
+}
+
+// Start a Wi-Fi access point
+void startWiFi() {
+  // Start the access point
+  WiFi.softAP(ssid, password);
+  Serial.print("Access Point \"");
+  Serial.print(ssid);
+  Serial.println("\" started\r\n");
+
+  Serial.print("IP address:\t");
+  // Send the IP address of the ESP8266 to the computer
+  Serial.println(WiFi.softAPIP());
+}
+
+// Start a WebSocket server
+void startWebSocket() {
+  // start the websocket server
+  webSocket.begin();
+  // if there's an incomming websocket message, go to function 'webSocketEvent'
+  webSocket.onEvent(webSocketEvent);
+  Serial.println("WebSocket server started.");
+}
+
+// Start a HTTP server with a file read handler and an upload handler
+void startServer() {
+  server.on("/", handleRoot);  
+  server.onNotFound(handleNotFound);          // if someone requests any other file or page, go to function 'handleNotFound'
+
+  // start the HTTP server
+  server.begin();
+  Serial.println("HTTP server started.");
+}
+
+void handleRoot() {
+  String htmlPage = webpageCode; 
+  server.send(200, "text/html", htmlPage);
+}
+
+// if the requested file or page doesn't exist, return a 404 not found error
+void handleNotFound(){
+  server.send(404, "text/plain", "404: File Not Found");
+}
+
+// When a WebSocket message is received
+void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t lenght) {
+  switch (type) {
+    // if the websocket is disconnected
+    case WStype_DISCONNECTED:            
+      Serial.printf("[%u] Disconnected!\n", num);
+      break;
+    // if a new websocket connection is established
+    case WStype_CONNECTED: {             
+        IPAddress ip = webSocket.remoteIP(num);
+        Serial.printf("[%u] Connected from %d.%d.%d.%d url: %s\n", num, ip[0], ip[1], ip[2], ip[3], payload);
+      }
+      break;
+    // if new text data is received
+    case WStype_TEXT:                    
+      Serial.printf("[%u] get Text: %s\n", num, payload);
+      if ( strcmp( (const char *)payload, rFW)==0 ) {
+        // move forward
+        Serial.println("Move forward");
+        L_MOTOR->run(FORWARD);
+        R_MOTOR->run(FORWARD);
+      } else if ( strcmp((const char *)payload, rBW)==0 ) {
+        // move backward
+        Serial.println("Move backward");
+        L_MOTOR->run(BACKWARD);
+        R_MOTOR->run(BACKWARD);
+      } else if (strcmp((const char *)payload, rLT)==0) {
+        // turn left
+        Serial.println("Turn left");
+        L_MOTOR->run(RELEASE);
+        R_MOTOR->run(FORWARD);
+      } else if (strcmp((const char *)payload, rRT)==0) {
+        // turn right
+        Serial.println("Turn right");
+        L_MOTOR->run(FORWARD);
+        R_MOTOR->run(RELEASE);
+      } else if (strcmp((const char *)payload, rST)==0) {
+        // halt the robot
+        Serial.println("Halting");
+        L_MOTOR->run(RELEASE);
+        R_MOTOR->run(RELEASE);
+      }
+      R_MOTOR->setSpeed(150);
+      L_MOTOR->setSpeed(150);
+      break;
+    default:
+      Serial.printf("Invalid WStype [%d]\r\n", type);
+      break;
+  }
+}
+
+// SETUP and LOOP
+// the pins with LEDs connected are outputs
+void setup() {
+  Serial.begin(9600);
+
+  // start AFMS and warm up motors
+  // create with the default frequency 1.6KHz
+  AFMS.begin();  
+  startMotors();
+  
+  // Start the Serial communication to send messages to the computer
+  Serial.begin(115200);
+
+  // configure the ESP8266 in soft-Access Point
+  startWiFi();
+  
+  // Start a WebSocket server
+  startWebSocket();
+  
+  // Start a HTTP server
+  startServer();
+}
+
+void loop() {
+  // constantly check for websocket events
+  webSocket.loop();
+
+  // run the server
+  server.handleClient();
+}
